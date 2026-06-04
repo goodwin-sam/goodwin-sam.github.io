ba45257c86df6070decddc29a8dbe39bd4738230
goodwin-sam <berrystump@protonmail.com>
2026-06-04 12:40:50 -0400

    feat: update resume link and add view option

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,8 +24,8 @@ const greeting = {
     "Magna Cum Laude Computer Science graduate passionate about systems programming, computer vision, and IoT. I love designing clean, efficient, and well-documented solutions using Go, Java, Python, and C++."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
-  displayGreeting: true // Set false to hide this section, defaults to true
+    "https://raw.githubusercontent.com/goodwin-sam/Resume/main/Sam-Goodwin_Resume.pdf", 
+  displayGreeting: true
 };
 
 // Social Media Links
